d8c3a28fa48c41ee601e0af77d0b3f3f802e52b7
Your Name <tejaszanzane2001@gmail.com>
2022-06-09 00:50:01 +0530

    update

--- js/scipt.js
+++ js/scipt.js
@@ -1,6 +1,6 @@
 // Typing animation
 var typed = new Typed(".typing",{
-    strings:["Front-End Developer","Back-End Developer"],
+    strings:["Web Developer","Back-End Developer","front-End Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
